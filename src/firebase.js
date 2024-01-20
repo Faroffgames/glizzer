5777116b410562ce1ddf21f2337516c06fd96616
TMC <127975346+Faroffgames@users.noreply.github.com>
2024-01-20 13:07:53 -0700

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -5,15 +5,16 @@ import { getAuth } from "firebase/auth";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
-// Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "YOUR FIREBASE API KEY",
-  authDomain: "YOUR FIREBASE AUTHDOMAIN",
-  projectId: "YOUR FIREBASE PROJECTID",
-  storageBucket: "YOUR FIREBASE STORAGE BUCKET",
-  messagingSenderId: "YOUR FIREBASE MESSAGESENDER ID",
-  appId: "YOUR FIREBASE APPID"
+  apiKey: "AIzaSyBJua2UY63jmK6OnzYg2Z7U-Qn7m4kG5-k",
+  authDomain: "popj-2af49.firebaseapp.com",
+  projectId: "popj-2af49",
+  storageBucket: "popj-2af49.appspot.com",
+  messagingSenderId: "842711373664",
+  appId: "1:842711373664:web:1cd6b90de6bcaab4b67005",
+  measurementId: "G-Q6XKRPLYM3"
 };
+
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
